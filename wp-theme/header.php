e3b8b62400edf5ff8a4777b941542a35102f9514
LAPTOP-FHTJEKIM\DUY <nguyenhoang.duy154@gmail.com>
2021-03-31 22:11:24 +0700

    update header-footer

--- wp-theme/header.php
+++ wp-theme/header.php
@@ -5,18 +5,6 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <link rel="profile" href="http://gmgp.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <script type='text/javascript' src='http://code.jquery.com/jquery.min.js'></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Sansita:ital,wght@1,700&display=swap" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/album/">
-    <link rel="stylesheet" href="./assets/css/flickity.css" media="screen">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="./assets/css/style_tmp.css">
-    <link rel="stylesheet" href="./assets/css/tintuc.css">
     <?php wp_head(); ?>
 </head>
 
