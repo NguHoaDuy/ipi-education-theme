5ff78f4587d60038625a6cbf7ed122a9ea0fa101
NguHoaDuy <nguyenhoang.duy154@gmail.com>
2021-04-15 21:00:41 +0700

    update footer

--- wp-theme/footer.php
+++ wp-theme/footer.php
@@ -1,154 +1,139 @@
-<div class="footer">
-    <footer class="page-footer font-small mdb-color pt-4">
-        <div class="container text-center text-md-left">
-            <div class="row text-center text-md-left mt-3 pb-3">
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold" style="color: black">Pháp Lý</h5>
-                    <hr class="hr4">
-                    <p>
-                        <a href="#" style="color: white">Các Điều Kiện & Điều Khoản</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Điều Lệ Vận Chuyển</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Bảo Mật Thông Tin</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Điều Kiện Sử Dụng Cookies</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Quy Chế Hoạt Động Sàn TMĐT</a>
-                    </p>
-                </div>
-                <hr class="w-100 clearfix d-md-none">
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold" style="color: black">Câu Hỏi</h5>
-                    <hr class="hr4">
-                    <p>
-                        <a href="#" style="color: white">Giới Thiệu</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Điều Khoản Sử Dụng</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Chính Sách Bảo Mật</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Hướng Dẫn Thanh Toán</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Hướng Dẫn Đặt Vé</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Cẩm Nang Du Lịch</a>
-                    </p>
-                </div>
-                <hr class="w-100 clearfix d-md-none">
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold" style="color: black">Hỗ Trợ</h5>
-                    <hr class="hr4">
-                    <p>
-                        <a href="#" style="color: white">Chăm Sóc Khách Hàng</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Chính Sách Bảo Vệ Khách Hàng</a>
-                    </p>
-                </div>
-                <hr class="w-100 clearfix d-md-none">
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold" style="color: black">Thông Tin</h5>
-                    <hr class="hr4">
-                    <p>
-                        <a href="#" style="color: white">Thông Tin Hành Lý</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Thuế,Phí,Lệ Phí & Phụ Thu</a>
-                    </p>
-                </div>
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold" style="color: black">Hãng Bay</h5>
-                    <hr class="hr4">
-                    <p>
-                        <a href="#" style="color: white">VietNam Airlines</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Vietjet Air</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Jetstart</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Bambo Airlines</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">United Airlines</a>
-                    </p>
-                    <p>
-                        <a href="#" style="color: white">Air Asia</a>
-                    </p>
-                </div>
-                <hr class="w-100 clearfix d-md-none">
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold" style="color: black">Liên Hệ</h5>
-                    <hr class="hr4">
-                    <p>
-                        <i class="fas fa-home mr-3" style="color: white">
-                            CodeGymDN
-                        </i>
-                    </p>
-                    <p>
-                        <i class="fas fa-envelope mr-3" style="color: white">
-                            Cgdn@gmail.com
-                        </i>
-                    </p>
-                    <p>
-                        <i class="fas fa-phone mr-3" style="color: white">
-                            + 01 234 567 88
-                        </i>
-                    </p>
-                    <p>
-                        <i class="fas fa-print mr-3" style="color: white">
-                            + 01 234 567 89
-                        </i>
-                    </p>
-                </div>
-            </div>
-            <hr class="hr2">
-            <div class="row d-flex align-items-center">
-                <div class="col-md-7 col-lg-8">
-                    <p class="text-center text-md-left" style="color: white">© 2020 C0320G1</p>
-                </div>
-                <div class="col-md-5 col-lg-4 ml-lg-0">
-                    <div class="text-center text-md-right">
-                        <ul class="list-unstyled list-inline">
-                            <li class="list-inline-item">
-                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
-                                    <i class="fab fa-facebook-f"></i>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
-                                    <i class="fab fa-twitter"></i>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
-                                    <i class="fab fa-google-plus-g"></i>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a class="btn-floating btn-sm rgba-white-slight mx-1">
-                                    <i class="fab fa-linkedin-in"></i>
-                                </a>
-                            </li>
-                        </ul>
+    <div class="footer">
+        <footer class="text-center text-dark">
+            <!-- Grid container -->
+            <div class="container p-4">
+                <section class="mb-4">
+                    <!-- Facebook -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    ><i class="fab fa-facebook-f"></i
+                    ></a>
+                    <!-- Twitter -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    ><i class="fab fa-twitter"></i
+                    ></a>
+                    <!-- Google -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    ><i class="fab fa-google"></i
+                    ></a>
+                    <!-- Instagram -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    ><i class="fab fa-instagram"></i
+                    ></a>
+                    <!-- Linkedin -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    ><i class="fab fa-linkedin-in"></i
+                    ></a>
+                    <!-- Github -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                    ><i class="fab fa-github"></i
+                    ></a>
+                </section>
+                <section class="">
+                    <form action="">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col-auto">
+                                <p class="pt-2">
+                                    <strong>Hãy gia nhập ngay hôm nay</strong>
+                                </p>
+                            </div>
+                            <div class="col-md-5 col-12">
+                                <div class="form-outline form-white mb-4">
+                                    <input type="email" id="form5Example2" class="form-control"
+                                           placeholder="Email address"/>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <button type="submit" class="btn btn-outline-light mb-4">
+                                    Subscribe
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </section>
+                <section class="mb-4">
+                    <strong>
+                        Trung tâm ngoại ngữ ABC
+                    </strong>
+                </section>
+                <section class="">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase">Về chúng tôi</h5>
+                            <ul class="list-unstyled mb-0">
+                                <li>
+                                    <a href="#!" class="text-dark">Tổng đài hỗ trợ</a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="text-dark">Ưu đãi khóa học</a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="text-dark">Đội ngũ giảng dạy</a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="text-dark">Lịch khai giảng</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase">Tin tức sự kiện</h5>
+                            <ul class="list-unstyled mb-0">
+                                <li>
+                                    <a href="#!" class="text-dark">Cơ hội nghề nghiệp</a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="text-dark">Liên hệ</a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="text-dark">Chính sách liên kết</a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="text-dark">Thông tin trung tâm</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase">Khóa học</h5>
+                            <ul class="list-unstyled mb-0">
+                                <li>
+                                    <a href="#!" class="text-dark">Khóa học phù hợp</a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="text-dark">Tiếng anh trẻ em</a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="text-dark">Tiếng anh học sinh</a>
+                                </li>
+                                <li>
+                                    <a href="#!" class="text-dark">Tiếng anh người lớn</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                            <h5 class="text-uppercase">Hotline</h5>
+                            <ul class="list-unstyled mb-0">
+                                <li>
+                                    <p class="text-dark">Hà Nội: 0388123456</p>
+                                </li>
+                                <li>
+                                    <p class="text-dark">Đà Nẵng: 0234456789</p>
+                                </li>
+                                <li>
+                                    <p class="text-dark">TP.HCM: 0399234567</p>
+                                </li>
+                                <li>
+                                    <img src="https://vus.edu.vn/Assets/images/dathongbao.png" style="width: 120px">
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
+                </section>
             </div>
+        </footer>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © 2021 Khóa học trực tuyến:
+            <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
         </div>
-    </footer>
-</div>
+    </div>
 </div>
 <?php wp_footer(); ?>
 
