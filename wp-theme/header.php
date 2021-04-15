00f79a6cc3e36b9ebc02d05750abfa5aa6fb2e9a
NguHoaDuy <nguyenhoang.duy154@gmail.com>
2021-04-15 21:16:16 +0700

    udpate header

--- wp-theme/header.php
+++ wp-theme/header.php
@@ -21,7 +21,19 @@
                     <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
-            <div class="col-md-7 row">
+            <div class="col-md-4 row" style="background-color: #88CC88">
                 <?php ipi_menu('primary-menu'); ?>
+            </div>
+            <div class="col-md-3" style="background-color: #0093c8">
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <button type="button" class="btn btn-secondary">Đăng kí</button>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" style="background-color: #1c232e; float: right">Đăng nhập</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
